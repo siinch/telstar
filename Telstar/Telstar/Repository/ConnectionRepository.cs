f561a99fc14146d21d7813a5525bd3805e8c8bdd
Simon Ingemann Bach <siba@netcompany.com>
2022-12-01 19:27:49 +0100

    City data

---
+++ Telstar/Telstar/Repository/ConnectionRepository.cs
@@ -0,0 +1,24 @@
+﻿using Telstar.Models;
+
+namespace Telstar.Repository
+{
+    public class ConnectionRepository
+    {
+        private readonly List<InternalConnection> internalConnections = new List<InternalConnection>
+        {
+            new InternalConnection
+            {
+                Id = new Guid(),
+                FromCity = new City {
+                    Id = 30,
+                    Name = "Tanger"
+                },
+                ToCity= new City
+                {
+                    Id = 
+                }
+            }
+        };
+
+    }
+}
