a2c575986c03b353bc7687a6bf19b3fd06c663a5
Sam Hollow <33451253+sambnh@users.noreply.github.com>
2022-12-01 17:25:17 +0100

    Change type of Models.City.Id from Guid to int

--- Telstar/Telstar/Models/City.cs
+++ Telstar/Telstar/Models/City.cs
@@ -2,7 +2,7 @@ namespace Telstar.Models
 {
     public class City
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         
         public string Name { get; set; }
 
