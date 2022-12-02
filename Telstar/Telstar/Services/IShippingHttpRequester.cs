2e5b49febe6a1f5ce577a698c6cd2aebbe72da8f
Sam Hollow <33451253+sambnh@users.noreply.github.com>
2022-12-02 01:11:59 +0100

    Add outbound integration request services

---
+++ Telstar/Telstar/Services/IShippingHttpRequester.cs
@@ -0,0 +1,9 @@
+﻿using Telstar.Models.Integration;
+
+namespace Telstar.Services
+{
+    public interface IShippingHttpRequester
+    {
+        public Task<Costs> OutboundShippingRequest(List<Parcel> parcelList, int startCityId, int destinationCityId);
+    }
+}
