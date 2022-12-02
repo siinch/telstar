a60d7068c8d1fd1ce4df4d1dd531102cb57ce91f
Sam Hollow <33451253+sambnh@users.noreply.github.com>
2022-12-02 02:21:43 +0100

    Add IIntegrationRequestAdapter interface and clarification comments on Parcel objects

---
+++ Telstar/Telstar/Models/Integration/IIntegrationRequestAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace Telstar.Models.Integration
+{
+    public interface IIntegrationRequestAdapter
+    {
+        public Booking toInternalBooking(IntegrationRequest request);
+
+        public IntegrationRequest toBooking(Booking booking);
+        
+    }
+}
