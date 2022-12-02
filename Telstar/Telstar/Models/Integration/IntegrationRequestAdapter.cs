9287f02b450d41bb27593ad125e0e88c99ecd4b0
Sam Hollow <33451253+sambnh@users.noreply.github.com>
2022-12-02 09:48:01 +0100

    WIP -  Integration adapter implementation

---
+++ Telstar/Telstar/Models/Integration/IntegrationRequestAdapter.cs
@@ -0,0 +1,26 @@
+﻿namespace Telstar.Models.Integration
+{
+    public class IntegrationRequestAdapter : IIntegrationRequestAdapter
+    {
+        public Booking toInternalBooking(IntegrationRequest request)
+        {
+            return new Booking()
+            {
+                Id = new Guid(),
+                FromCity = new City(),
+                ToCity = new City(),
+                Parcels = new List<Parcel>
+                {
+
+                }
+]
+            }
+        }
+
+        public IntegrationRequest toBooking(Booking booking)
+        {
+
+        }
+        
+    }
+}
